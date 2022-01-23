e15eb6e548a71514c1306a2f8a665f22021c5566
Joshua <jdkelly2015@gmail.com>
2022-01-22 20:34:21 -0500

    made trivia questions and answers. now i need to import pictures for each question

---
+++ assets/javascript/script.js
@@ -0,0 +1,48 @@
+// Trivia Array
+const triviaQuestions = [
+  {
+    question: "Where does Super Mario World take place?",
+    options: [
+      { a: "The Light Forest", correct: false },
+      { b: "Dinosaur Land", correct: true },
+      { c: "Grandmaster Galaxy", correct: false },
+      { d: "New York", correct: false },
+    ],
+  },
+  {
+    question: "Who kidnaps Princess Daisy?",
+    options: [
+      { a: "Tatanga", correct: true },
+      { b: "Bowser", correct: false },
+      { c: "Wario", correct: false },
+      { d: "Yoshi", correct: false },
+    ],
+  },
+  {
+    question: "Who kidnaps Princess Peach?",
+    options: [
+      { a: "Tatanga", correct: false },
+      { b: "Bowser Jr", correct: false },
+      { c: "Waluigi", correct: false },
+      { d: "Bowser", correct: true },
+    ],
+  },
+  {
+    question: "Who is on the castle roof in Super Mario 64?",
+    options: [
+      { a: "Donkey Kong", correct: false },
+      { b: "Princess Peach", correct: false },
+      { c: "Yoshi", correct: true },
+      { d: "Mario", correct: false },
+    ],
+  },
+  {
+    question: "Who guards the Fortress in Super Mario 64?",
+    options: [
+      { a: "Boom Boom", correct: true },
+      { b: "Chain Chomp", correct: false },
+      { c: "Lava Queen", correct: false },
+      { d: "2 Toads", correct: false },
+    ],
+  },
+];
